c5f83e3fcde2a17346e84b90ba098b6d46792c87
Exodo <69496221+Exodo0@users.noreply.github.com>
2024-01-29 14:01:53 -0600

    Fix Cooldown and more.

--- Events/Ranking.js
+++ Events/Ranking.js
@@ -6,18 +6,19 @@ const cooldown = new Set();
 
 module.exports = {
   name: "messageCreate",
-  description: "Ranking system Event.",
+  description: "Ranking system",
 
   async execute(message, client) {
     const guildId = message.guild.id;
     const userId = message.author.id;
 
-    if (message.author.bot || !message.guild) return;
+    if (message.author.bot || !message.guild || cooldown.has(userId)) return;
 
     let user;
+    let rankingChannel;
 
     try {
-      let rankingChannel = await Notify.findOne(
+      rankingChannel = await Notify.findOne(
         { GuildId: guildId },
         { ChannelId: 1, Status: 1 }
       );
@@ -37,79 +38,81 @@ module.exports = {
         await rankingChannel.save();
         return;
       }
+    } catch (err) {
+      console.log(err);
+    }
 
-      const xpAmount = Math.floor(Math.random() * (25 - 15 + 1) + 15);
+    const xpAmount = Math.floor(Math.random() * (25 - 15 + 1) + 15);
 
-      user = await UserLevel.findOneAndUpdate(
+    user = await UserLevel.findOneAndUpdate(
+      {
+        GuildId: guildId,
+        UserId: userId,
+      },
+      {
+        $inc: { Xp: xpAmount },
+        $setOnInsert: { Level: 0 },
+      },
+      { upsert: true, new: true }
+    );
+
+    let { Xp, Level } = user;
+
+    if (Xp >= Level * 100) {
+      cooldown.add(userId);
+
+      ++Level;
+      Xp = 0;
+
+      let notificationChannel = null;
+
+      if (rankingChannel && rankingChannel.ChannelId !== null) {
+        try {
+          notificationChannel = await client.channels.fetch(
+            rankingChannel.ChannelId
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      if (!notificationChannel) {
+        notificationChannel = message.channel;
+      }
+
+      const buffer = await profileImage(userId, {
+        customTag: `You level up to: ${Level}!`,
+      });
+
+      const attachment = new AttachmentBuilder(buffer, {
+        name: "profile.png",
+      });
+
+      const embed = new EmbedBuilder()
+        .setImage("attachment://profile.png")
+        .setColor("Aqua")
+        .setTimestamp();
+
+      notificationChannel.send({
+        embeds: [embed],
+        files: [attachment],
+      });
+
+      await UserLevel.updateOne(
         {
           GuildId: guildId,
           UserId: userId,
         },
         {
-          $inc: { Xp: xpAmount },
-          $setOnInsert: { Level: 0 },
-        },
-        { upsert: true, new: true }
+          Level: Level,
+          Xp: Xp,
+        }
       );
 
-      let { Xp, Level } = user;
-
-      if (Xp >= Level * 100) {
-        ++Level;
-        Xp = 0;
-
-        let notificationChannel = null;
-
-        if (rankingChannel && rankingChannel.ChannelId !== null) {
-          try {
-            notificationChannel = await client.channels.fetch(
-              rankingChannel.ChannelId
-            );
-          } catch (err) {
-            console.log(err);
-          }
-        }
-
-        if (!notificationChannel) {
-          notificationChannel = message.channel;
-        }
-
-        const buffer = await profileImage(userId, {
-          customTag: `You level up to: ${Level}!`,
-        });
-
-        const attachment = new AttachmentBuilder(buffer, {
-          name: "profile.png",
-        });
-
-        const embed = new EmbedBuilder()
-          .setImage("attachment://profile.png")
-          .setColor("Aqua")
-          .setTimestamp();
-
-        notificationChannel.send({
-          embeds: [embed],
-          files: [attachment],
-        });
-
-        await UserLevel.updateOne(
-          {
-            GuildId: guildId,
-            UserId: userId,
-          },
-          {
-            Level: Level,
-            Xp: Xp,
-          }
-        );
-      }
-    } catch (err) {
-      console.log(err);
+      setTimeout(() => {
+        cooldown.delete(userId);
+      }, 60000);
     }
-    cooldown.add(message.author.id);
-
-    setTimeout(() => {
-      cooldown.delete(message.author.id);
-    }, 60 * 1000);
   },
 };
+
