28c2690b0f46f3715413b6d0feac5f16979af9a8
Exodo <69496221+Exodo0@users.noreply.github.com>
2024-02-12 15:10:18 -0600

    ⚙️ Fix error while excute.
    Force discord avatar from: webp, jpg, etc.  to: png

--- Commands/leaderboard.js
+++ Commands/leaderboard.js
@@ -16,56 +16,61 @@ module.exports = {
    * @param {Client} client
    */
   async execute(interaction, client) {
-    await interaction.deferReply();
+    try {
+      await interaction.deferReply();
 
-    const users = await UserLevel.find({
-      GuildId: interaction.guild.id,
-    }).sort({ Level: -1 });
+      const users = await UserLevel.find({
+        GuildId: interaction.guild.id,
+      }).sort({ Level: -1 });
 
-    if (users.length === 0) {
-      await interaction.followUp({
-        content: "There are no users in the ranking yet.",
+      if (users.length === 0) {
+        await interaction.followUp({
+          content: "There are no users in the ranking yet.",
+        });
+        return;
+      }
+
+      const usersData = await Promise.all(
+        users.map(async (user, index) => {
+          const fetchedUser = await client.users.fetch(user.UserId);
+          return {
+            top: index + 1,
+            tag: fetchedUser.username,
+            avatar: fetchedUser.displayAvatarURL({
+              format: "png",
+              dynamic: true,
+            }),
+            score: user.Level.toString(),
+          };
+        })
+      ).catch((error) => {
+        console.error(error);
       });
-      return;
+
+      const top = await new canvafy.Top()
+        .setOpacity(0.5)
+        .setScoreMessage("Level")
+        .setBackground(
+          "image",
+          "https://img.freepik.com/free-photo/ultra-detailed-nebula-abstract-wallpaper-4_1562-749.jpg"
+        )
+        .setUsersData(usersData)
+        .build();
+
+      await interaction.followUp({
+        files: [
+          {
+            attachment: top,
+            name: `top-${interaction.user.id}.png`,
+          },
+        ],
+      });
+    } catch (error) {
+      console.error(error);
+      interaction.followUp({
+        content: "There was an error while executing this command.",
+        ephemeral: true,
+      });
     }
-
-    const usersData = await Promise.all(
-      users.map(async (user, index) => {
-        const fetchedUser = await client.users.fetch(user.UserId);
-        return {
-          top: index + 1,
-          avatar: `https://cdn.discordapp.com/avatars/${user.UserId}/${fetchedUser.avatar}.png`,
-          tag: fetchedUser.username,
-          score: user.Level.toString(),
-        };
-      })
-    );
-
-    const top = await new canvafy.Top()
-      .setOpacity(0.5)
-      .setScoreMessage("Level")
-      .setBackground(
-        "image",
-        "https://img.freepik.com/free-photo/ultra-detailed-nebula-abstract-wallpaper-4_1562-749.jpg"
-      )
-      .setColors({
-        box: "#212121",
-        username: "#ffffff",
-        score: "#ffffff",
-        firstRank: "#f7c716",
-        secondRank: "#9e9e9e",
-        thirdRank: "#94610f",
-      })
-      .setUsersData(usersData)
-      .build();
-
-    await interaction.followUp({
-      files: [
-        {
-          attachment: top,
-          name: `top-${interaction.user.id}.png`,
-        },
-      ],
-    });
   },
 };
